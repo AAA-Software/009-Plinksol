03301862ceff052ca6634ceb8602ec9dcfc0982a
Braimah-Abiola <braimahabiola.official@gmail.com>
2024-04-02 13:55:49 +0000

    Updates

--- src/components/ui/presale-calculator.tsx
+++ src/components/ui/presale-calculator.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import { Button } from "./button";
 import { Progress } from "./progress";
+import { SparklesPreview } from "./sparkles";
 
 const PresaleCalculator = () => {
   return (
@@ -23,26 +24,29 @@ const PresaleCalculator = () => {
           <div className="flex flex-col w-full mt-4">
             <Progress className="h-2" value={5} />
             <div className="w-full flex py-3 flex-row justify-between text-white font-sora font-normal">
-              <h4 className=" opacity-70">USD Raised</h4>
-              <p>$ 1,000</p>
+              <h4 className=" opacity-70">SOL Raised</h4>
+              <div className="flex flex-row gap-2 items-center">
+                <Image
+                  height={20}
+                  width={20}
+                  className=" object-contain"
+                  src="/img/solana.png"
+                  alt="Solana"
+                />
+                <p>1,000</p>
+              </div>
             </div>
           </div>
 
-          <div className="w-full h-32 border border-white/10 rounded-xl"></div>
+          <div className="w-full h-[12rem] border border-white/10 rounded-xl overflow-hidden">
+            <SparklesPreview />
+          </div>
 
-          <Button className="w-full py-6 mt-6">Proceed to Checkout</Button>
+          {/* <Button className="w-full py-6 mt-6">Proceed to Checkout</Button> */}
         </div>
         <div className=" border-dashed border-t border-t-white/15 w-[90%]" />
         <div className="bg-[#141826] rounded-2xl flex items-center justify-center py-4 h-20 w-full">
-          <div className=" relative w-full h-[32px] hover:scale-110 transition-all ease-in-out">
-            <Image
-              fill
-              quality={100}
-              src="/img/coins.png"
-              className=" object-contain object-center"
-              alt="Crypto"
-            />
-          </div>
+          {""}
         </div>
       </div>
     </div>
