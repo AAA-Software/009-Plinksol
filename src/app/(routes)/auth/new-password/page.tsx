179a96a93fa3295169a5977f393d52a3cb724e27
Braimah-Abiola <braimahabiola.official@gmail.com>
2024-04-04 14:42:32 +0000

    Suspense

--- src/app/(routes)/auth/new-password/page.tsx
+++ src/app/(routes)/auth/new-password/page.tsx
@@ -1,10 +1,13 @@
 import { NewPasswordForm } from "@/components/auth/new-password-form";
+import { Suspense } from "react";
 
 const NewPasswordPage = () => {
   return (
-    <div className="h-[100vh] flex justify-center items-center">
-      <NewPasswordForm />
-    </div>
+    <Suspense fallback={<div>Loading...</div>}>
+      <div className="h-[100vh] flex justify-center items-center">
+        <NewPasswordForm />
+      </div>
+    </Suspense>
   );
 };
 
